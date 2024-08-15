e774ee15eb55ee41ed9112333dbf388efb60839e
Guilherme Moreira <gmoreira0101@gmail.com>
2024-08-15 18:21:58 -0300

    Commit teste js

--- HTML/script.js
+++ HTML/script.js
@@ -1,12 +1,33 @@
-const formulario = document.querySelector("form");
-const Inrprensa = document.querySelector(".numprensa");
-const Inrprensada = document.querySelector(".nrprensada");
-const Inrcavidade = document.querySelector(".nrcavidade");
-const Iidpeca = document.querySelector(".idpeca");
-const Inrpeca = document.querySelector(".nrpeca");
-const hoje = new Date().toISOString().split('T')[0];
+function cadastrar(formularioId) {
+    const formulario = document.getElementById(formularioId);
+    console.log(formulario);
+    if (!formulario) {
+        console.error(`Formulário com ID ${formularioId} não encontrado.`);
+        return;
+    }
 
-function cadastrar() {
+    const Inrprensa = formulario.querySelector('input[name="numeroPrensa"]:not([type="hidden"])');
+    console.log("Inrprensa encontrado:", Inrprensa!== null);
+
+    const Inrprensada = formulario.querySelector('input[name="nrprensada"]');
+    console.log("Inrprensada encontrado:", Inrprensada !== null);
+
+    const Inrcavidade = formulario.querySelector('input[name="nrcavidade"]');
+    console.log("Inrcavidade encontrado:", Inrcavidade !== null);
+
+    const Iidpeca = formulario.querySelector('input[name="idpeca"]');
+    console.log("Iidpeca encontrado:", Iidpeca !== null);
+
+    const Inrpeca = formulario.querySelector('input[name="nrpeca"]');
+    console.log("Inrpeca encontrado:", Inrpeca !== null);
+
+    if (!Inrprensa || !Inrprensada || !Inrcavidade || !Iidpeca || !Inrpeca) {
+        console.error("Um ou mais campos do formulário não foram encontrados.");
+        return;
+    }
+
+    const hoje = new Date().toISOString().split('T')[0];
+
     fetch("http://localhost:8080/cadastrar", {
         headers: {
             'Accept': 'application/json',
@@ -22,24 +43,11 @@ function cadastrar() {
             data: hoje
         })
     })
-    .then(function (res) { console.log(res) })
-    .catch(function (res) { console.log(res) })
+    .then(function (res) { 
+        console.log(res);
+        limpar(formulario); 
+    })
+    .catch(function (res) { 
+        console.log(res); 
+    });
 }
-
-function limpar() {
-    Inrprensada.value = "";
-    Inrcavidade.value = "";
-    Iidpeca.value = "";
-    Inrpeca.value = "";
-}
-
-formulario.addEventListener('submit', function (event) {
-    event.preventDefault();
-    console.log(Inrprensa.value)
-    console.log(Inrprensada.value)
-    console.log(Inrcavidade.value)
-    console.log(Inrpeca.value)
-    console.log(Iidpeca.value)
-    cadastrar();
-    limpar();
-})
